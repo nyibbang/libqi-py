626b6f60537fe3781aa9a349f43138f1c2f756f3
Vincent PALANCHER <vincent.palancher@gmail.com>
2025-06-05 16:59:35 +0200

    Format module file

--- src/qi_py/__init__.py
+++ src/qi_py/__init__.py
@@ -1,5 +1,10 @@
 from .logging import Logger, error, fatal, info, verbose, warning
 
 __all__ = [
-  "Logger", "error", "fatal", "info", "verbose", "warning",
+    "Logger",
+    "error",
+    "fatal",
+    "info",
+    "verbose",
+    "warning",
 ]
